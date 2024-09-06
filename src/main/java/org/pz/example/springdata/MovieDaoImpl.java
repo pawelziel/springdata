b53dbcd009aa48ebfdbc893c235e82bde4ae1b00
apz012 <apz012@motorolasolutions.com>
2024-09-06 22:00:51 +0200

    DAO done

---
+++ src/main/java/org/pz/example/springdata/MovieDaoImpl.java
@@ -0,0 +1,32 @@
+package org.pz.example.springdata;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Transactional
+@Repository
+public class MovieDaoImpl implements MovieDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void addMovie(Movie movie) {
+        entityManager.persist(movie);
+    }
+
+    @Override
+    public void deleteMovie(int id) {
+        Movie movie = entityManager.find(Movie.class, id);
+        if (movie !=null)
+            entityManager.remove(movie);
+    }
+
+    @Override
+    public List<Movie> getAllMovies() {
+        return entityManager.createQuery("from Movie", Movie.class).getResultList();
+    }
+}
